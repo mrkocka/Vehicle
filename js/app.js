9fa78cc95c690dab87d8b1d7d8027ae5a4008e8f
Mr. Joe <radics.pepe@gmail.com>
2022-10-20 22:37:14 +0200

    save_1_10.20

--- js/app.js
+++ js/app.js
@@ -1,10 +1,20 @@
 let vehicleCont = document.getElementById("js-container");
 let vbtn = vehicleCont.getElementsByClassName("vi-button");
+let sgk = document.getElementById("js-szgk");
 
- for (var i = 0; i < vbtn.length; i++) {
+for (var i = 0; i < vbtn.length; i++) {
   vbtn[i].addEventListener("click", function () {
     var current = document.getElementsByClassName("active");
     current[0].className = current[0].className.replace(" active", "");
     this.className += " active";
   });
 }
+
+
+ sgk.addEventListener("click", testvalue);
+
+function testvalue() {
+  console.log("Király!");
+  let vinumb = sgk.value; 
+  console.log(vinumb);
+} 
